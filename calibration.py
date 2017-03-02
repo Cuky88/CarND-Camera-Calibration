506740bcd76c0dbbbc6b13f3f823f9dcf9689643
Manfred Diaz <takeitallsource@gmail.com>
2017-03-02 11:41:01 -0500

    [ADDED] Own camera calibration pipeline with commented output.

--- calibration.py
+++ calibration.py
@@ -2,12 +2,13 @@ import cv2
 import numpy as np
 import glob
 
+pattern_size = (8, 6)
 real_world_grid = np.zeros((6 * 8, 3), np.float32)
 real_world_grid[:, :2] = np.mgrid[0:8, 0:6].T.reshape(-1, 2)
 
 real_world_points = [] # 3d points in real world space
 transformed_points = []  # 2d points in image plane.
-pattern_size = (8, 6)
+
 
 calibration_images = glob.glob("calibration_wide/GO*.jpg")
 
@@ -25,12 +26,12 @@ test_image_size = (test_image.shape[1], test_image.shape[0])
 calibrated, camera_matrix, distortion_coefficients, rotation_vectors, translation_vectors = \
     cv2.calibrateCamera(real_world_points, transformed_points, test_image_size, cameraMatrix=None, distCoeffs=None)
 
-print("Camera Matrix: \n[fx 0 cx] [0 fy cy] [0 0 1] where:")
-print("\t(cx, cy) => principal point (center)")
-print("\t(fx, fy) => focal length in pixels\n")
+print("Camera Matrix \n\t[fx  0  cx] \n\t[0  fy  cy] \n\t[0   0   1]")
+print("(cx, cy) => principal point (center)")
+print("(fx, fy) => focal length in pixels\n")
 print(camera_matrix, "\n")
 
-print("Distortion Coefficients: \n[k1, k2, p1, p2, [k3, k4, k5, k6]] where: ")
+print("Distortion Coefficients: \n\t[k1, k2, p1, p2, [k3, k4, k5, k6]] where: ")
 print("\tk1, k2, ... => radial distortion coefficients.")
 print("\tp1, p2 => tangential distortion coefficients\n")
 print(distortion_coefficients, "\n")
